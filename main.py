296477df26f3639dfc1d9f02184645e4e737ee7a
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-04-16 16:44:08 -0300

    feat: file ingestion to output

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Olá, mundo!")
+import os
+
+# Pede o diretório ao usuário
+while True:
+    path = input('Digite o caminho do diretório: ')
+
+    if os.path.isdir(path):
+        break
+
+    print('Caminho inválido, tente novamente')
+
+# Arquivo de saída (no diretório autal do script)
+output_file = os.path.join(os.getcwd(), 'resultado.txt')
+
+with open(output_file, 'w', encoding='utf-8') as output:
+    for file in os.listdir(path):
+        full_path = os.path.join(path, file)
+
+        # Garante que é arquivo
+        if os.path.isfile(full_path):
+            try:
+                with open(full_path, 'r', encoding='utf-8') as f:
+                    output.write(f'-- {file} --\n')  # identifica o arquivo
+                    output.write(f.read())
+                    output.write('\n\n')  # separação entre arquivos
+
+                print(f'Adicionado: {file}')
+
+            except Exception as e:
+                print(f'Erro ao ler {file}: {e}')
+
+print(f'\nArquivo gerado em: {output_file}')
