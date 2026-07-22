84ccd3e13881c57ff4beee75551bea072661ac7c
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-07-22 13:38:10 -0300

    fix: fixes response value serialization error

--- src/routes/ingest_routes.py
+++ src/routes/ingest_routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 
-from services.ingest_service import IngestService
+from src.services.ingest_service import IngestService
 from src.controllers.ingest_controller import IngestController
 
 ingest_service = IngestService()
-ingest_controller = IngestController()
+ingest_controller = IngestController(ingest_service)
 ingest_bp: Blueprint = Blueprint('ingest_routes', __name__)
 
 
--- src/services/ingest_service.py
+++ src/services/ingest_service.py
@@ -1,6 +1,9 @@
+from src.responses.ingest_response import IngestResponse
 from src.dtos.ingest_request import IngestRequest
 
 
 class IngestService:
-    def ingest(self, dto: IngestRequest):
-        return 'Olá, mundo!'
+    def ingest(self, dto: IngestRequest) -> IngestResponse:
+        return IngestResponse(
+            message='Olá, mundo!',
+        )
