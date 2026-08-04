4b270c56c39257c6937b8c552d9dd067fbcc003e
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-08-04 17:20:11 -0300

    refactor: remove inconsistencies from the dtos

--- src/dtos/ingest_request_dto.py
+++ src/dtos/ingest_request_dto.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from src.enums.pattern_type_enum import PatternTypeEnum
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class IngestRequestDTO:
     path: str
     pattern_type: PatternTypeEnum = PatternTypeEnum.EXCLUDE
--- src/dtos/validation_error_item_dto.py
+++ src/dtos/validation_error_item_dto.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
 from typing import TypedDict
 
-@dataclass(frozen=True)
 class ValidationErrorItemDTO(TypedDict):
     location: list[str]
     message: str
