755a9d92ee528d0a78a634a2fe40ef59110393ce
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-08-11 14:57:25 -0300

    feat: adds ingest page

--- src/routes/web_routes.py
+++ src/routes/web_routes.py
@@ -1,6 +1,11 @@
-from flask import Blueprint
+from pathlib import Path
+
+from flask import Blueprint, send_from_directory
 
 from src.container.di_container import web_controller
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+ASSETS_PATH = PROJECT_ROOT / 'resources' / 'assets'
 
 web_bp = Blueprint('web_routes', __name__)
 
@@ -8,3 +13,11 @@ web_bp = Blueprint('web_routes', __name__)
 @web_bp.route('/', methods=['GET'])
 def index():
     return web_controller.index()
+
+
+@web_bp.route('/assets/<path:filename>', methods=['GET'])
+def assets(filename: str):
+    return send_from_directory(
+        ASSETS_PATH,
+        filename,
+    )
