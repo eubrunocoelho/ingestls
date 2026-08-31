0acf51318995583fefd5c4cdcf22a66d59e20c37
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-08-31 15:34:52 -0300

    chore: adds pattern exclusions for running the application

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from src.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        exclude_patterns=['*/tmp/*'],
+    )
